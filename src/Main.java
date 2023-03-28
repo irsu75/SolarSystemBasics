97dbf35070344e6a88aa7ef6c940ddf620d458c4
irsu75 <irinasukhanova01@gmail.com>
2023-03-28 19:22:06 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        SolarSystem solar=new SolarSystem();
+        solar.sun="1";
+        solar.moon="2";
+        solar.planets="3";
+    }
+}
